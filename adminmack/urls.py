11920c1d461f71e73fa194a3d731c2e9d627e4e0
marian <mariangonzalezcalderon@gmail.com>
2026-02-28 22:37:44 -0300

    Fix root url for render

--- adminmack/urls.py
+++ adminmack/urls.py
@@ -1,17 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
 from comisiones.views import ver_comprobantes, ver_saldos, graficos01, graficos02
-
+from django.shortcuts import redirect
 urlpatterns = [
 
-    path('admin/', admin.site.urls),
+    path('', lambda request: redirect('/saldos/')),  # 👈 ESTA LINEA SOLUCIONA TODO
 
+    path('admin/', admin.site.urls),
     path('comprobantes/', ver_comprobantes),
     path('saldos/', ver_saldos),
     path('graficos01/', graficos01),
     path('graficos02/', graficos02),
 
-    # ESTA línea conecta TODO comisiones.urls
     path('', include('comisiones.urls')),
-
 ]
