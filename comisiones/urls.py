7c6142c78240c1dae0b2c0717b58fff2e48c7521
marian <mariangonzalezcalderon@gmail.com>
2026-03-01 23:17:02 -0300

    grupo

--- comisiones/urls.py
+++ comisiones/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path("aseguradoras/",  views_importaciones.aseguradoras_view,  name="aseguradoras"),
     path("aseguradoras/activar/<int:id>/",     views_importaciones.activar_aseguradora,   name="activar_aseguradora"),
     path("aseguradoras/eliminar/<int:id>/",    views_importaciones.eliminar_o_desactivar_aseguradora,  name="eliminar_aseguradora"),
+    path("aseguradoras/actualizar-grupo/<int:id>/", views_importaciones.actualizar_grupo_aseguradora, name="actualizar_grupo_aseguradora"),    
     path("importaciones/comprobantes-arca/",   importar_comprobantes_arca_view, name="importar_comprobantes_arca" ),
     path("importaciones/cobranzas/", importar_cobranzas_view, name="importar_cobranzas"),
     path("grafico-indice-mensual/", views.grafico_indice_mensual, name="grafico_indice_mensual"),
@@ -19,5 +20,4 @@ urlpatterns = [
     path("parametros/editar/<int:id>/", views.parametro_editar_view, name="parametro_editar"),
     path("parametros/eliminar/<int:id>/", views.parametro_eliminar_view, name="parametro_eliminar"),
 ]
-
 
